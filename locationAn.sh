cfcf40bcc3489a98df1a60a07b143b7f21a3eb54
SamyEZE <sami.boudemagh@gmail.com>
2023-10-15 23:40:33 +0200

    le script fonctionne, on obtient 8720 locations en 2016

--- locationAn.sh
+++ locationAn.sh
@@ -3,7 +3,6 @@
 # Récupère l'année en tant qu'argument
 an=$1
 
-# Recherche 'Location' dans les fichiers de l'année spécifiée, puis compte le nombre d'occurrences
 nombrelocation=$(grep -r 'Location' ./$an_*_* | wc -l)
 
 # montre le result 
